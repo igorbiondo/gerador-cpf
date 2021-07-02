371352e83fb6188dd1540e538df17b0e37a51917
DESKTOP-F4071BP\igorb <igorbiondo@gmail.com>
2021-07-02 12:08:07 -0300

    Adding gerar button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import GeraCPF from './modules/GeraCPF';
 import './assets/css/style.css';
 
 (function () {
+	const gerar = document.querySelector('.gerar');
+	gerar.addEventListener('click', () => {
+		cpfGerado.innerHTML = gera.geraNovoCpf();
+	});
 	const gera = new GeraCPF();
 	const cpfGerado = document.querySelector('.cpf-gerado');
 	cpfGerado.innerHTML = gera.geraNovoCpf();
